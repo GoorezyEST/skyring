d3d05af4bf468fd0f2495688043903e59a6d5ce7
GoorezyEST <goorezyestudios@gmail.com>
2023-01-30 19:01:14 -0300

    Fixed chat input height

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: SignupComponent },
-];
+const routes: Routes = [{ path: '', component: SignupComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
